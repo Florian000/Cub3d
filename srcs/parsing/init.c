28afc435fd157da4cfc65b8d9ca597aef011fd44
Joseph Vittoz <jvittoz@paul-f3Br9s3.clusters.42paris.fr>
2024-11-24 15:29:00 +0100

    free err

--- srcs/parsing/init.c
+++ srcs/parsing/init.c
@@ -1,17 +1,5 @@
 #include "../../include/cub3d.h"
 
-//initializes the data strure
-int  init_data(void)
-{
-    t_data *data;
-
-    data = get_data();
-    ft_bzero(data, sizeof(t_data));
-    data->map = malloc(sizeof(t_map));
-    data->gd_args = NULL;
-    return (VALID);
-}
-
 int init_map(t_data *data)
 {
     t_map *map;
@@ -20,5 +8,18 @@ int init_map(t_data *data)
     map->hight = 0;
     map->length = 0;
     map->brut_map = NULL;
+    return (VALID);
+}
+
+//initializes the data strure
+int  init_data(void)
+{
+    t_data *data;
+
+    data = get_data();
+    ft_bzero(data, sizeof(t_data));
+    data->map = malloc(sizeof(t_map));
+    init_map(data);
+    data->gd_args = NULL;
     return (VALID);
 }
